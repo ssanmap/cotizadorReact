be850f94b4ce4fdea1dd4ef770a62bd6e5a3292c
ssanmap <sebastian.smp21@gmail.com>
2021-07-23 18:12:17 -0400

    f

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = ({titulo}) => {
+    return (
+        <div>
+            <header>
+
+            <h1>{titulo}</h1>
+            </header>
+        </div>
+    )
+}
+
+export default Header
