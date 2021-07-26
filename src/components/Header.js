59c23f1a466245f62484f48bbb9d61dc33cc1e41
ssanmap <sebastian.smp21@gmail.com>
2021-07-26 18:44:08 -0400

    o

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react'
+import styled from '@emotion/styled';
+
+const ContenedorHeader = styled.header`
+ background-color: #26C6DA;
+ padding:10px;
+ font-weight: bold;
+ color: #FFFFFF;
+`;
+const TextoHeader = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`;
 
 const Header = ({titulo}) => {
     return (
         <div>
-            <header>
+            <ContenedorHeader>
 
-            <h1>{titulo}</h1>
-            </header>
+            <TextoHeader>{titulo}</TextoHeader>
+            </ContenedorHeader>
         </div>
     )
 }
