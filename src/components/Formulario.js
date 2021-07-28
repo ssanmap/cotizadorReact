aaad76af2f411c37ec9be871ea8763371de9328d
ssanmap <sebastian.smp21@gmail.com>
2021-07-28 18:38:32 -0400

    o

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,21 +1,68 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
+
+
+const Campo = styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+`;
+const Label = styled.label`
+    flex: 0 0 100px;
+`;
+const Select = styled.select`
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;
+`;
+const InputRadio = styled.input`
+    margin: 0 1rem;
+`;
+
+const Boton = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+    &:hover {
+        background-color: #26C6DA;
+        cursor: pointer;
+    }
+`;
+
+const Error = styled.div`
+    background-color: red;
+    color: white;
+    padding: 1rem;
+    width:100%;
+    text-align: center;
+    margin-bottom: 2rem;
+`;
 
 const Formulario = () => {
     return (
         <div>
             <form action="">
-                <div>
-                    <label htmlFor="">Marca :</label>
-                    <select >
+                <Campo>
+                    <Label htmlFor="">Marca :</Label>
+                    <Select >
                         <option value="">--Selecione--</option>
                         <option value="americano">Americano</option>
                         <option value="europeo">Europeo</option>
                         <option value="asiatico">Asiatico</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="">Año :</label>
-                    <select >
+                    </Select>
+                </Campo>
+                <Campo>
+                    <Label htmlFor="">Año :</Label>
+                    <Select >
                     <option value="">-- Seleccione --</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
@@ -27,17 +74,17 @@ const Formulario = () => {
                     <option value="2014">2014</option>
                     <option value="2013">2013</option>
                     <option value="2012">2012</option>
-                    </select>
-                </div>
-                <div>
-                    <label htmlFor="">Plan :</label>
-                   <input type="radio" name="plan" value="basico" id="" />
+                    </Select>
+                </Campo>
+                <Campo>
+                    <Label htmlFor="">Plan :</Label>
+                   <InputRadio type="radio" name="plan" value="basico" id="" />
                    Basico
-                   <input type="radio" name="plan" value="completo" id="" />
-                   Basico
+                   <InputRadio type="radio" name="plan" value="completo" id="" />
+                   Completo
 
-                </div>
-                <button ></button>
+                </Campo>
+                <Boton type="button">Cotizar</Boton>
             </form>
         </div>
     )
